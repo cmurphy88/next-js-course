906894ea9a8284ad00ed1b516ee1e1be0b32f63a
conor-murphy <conor.murphy@harvardbusiness.org>
2025-03-06 16:57:40 +0000

    api routes complete

--- intro-v3-app/utils/actions.ts
+++ intro-v3-app/utils/actions.ts
@@ -1,5 +1,16 @@
 'use server'
+import { revalidatePath } from 'next/cache'
 import db from './db'
+
+export const completeTodo = async (id: string) => {
+  await db.todo.update({
+    where: { id },
+    data: {
+      completed: true,
+    },
+  })
+  revalidatePath('/todos')
+}
 
 export const newTodo = async (formData) => {
   const todo = await db.todo.create({
@@ -7,4 +18,6 @@ export const newTodo = async (formData) => {
       content: formData.get('content'),
     },
   })
+
+  revalidatePath('/todos')
 }
